19079679d4944ec37d58d39f5bcc7331f20c434c
aalkhub10 <alkhubabdalhadi10@gmail.com>
2026-06-15 13:18:38 +1000

    started card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,9 @@
+const Card = ({ children, className} : {children: React.ReactNode, className?: string}) => {
+    return (
+        <div className={className}>
+            {children}
+        </div>
+    );
+};
+
+export default Card;
--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,11 +1,24 @@
+import Card from '../components/Card';
+
 
 const HomeScreen = () => {
     return (
+        <div className="min-h-screen bg-slate-50 p-8">
         <header className="flex flex-row items-center justify-center pt-20">
-            <h1 className="text-5xl font-extrabold tracking-tight text-black mb-4 text-center">
+            <h1 className="font-kufi text-6xl font-extrabold tracking-tight text-black mb-4 text-center">
             Khawarizmia-Visualizer
             </h1>
         </header>
+        
+        <div className="grid grid-cols-4 gap-4" >
+
+        <Card>
+
+        </Card>
+
+        </div>
+       
+        </div>
     );
 };
 
