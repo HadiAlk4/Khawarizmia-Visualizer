9d3479e163b0c28a46db3a61f14d36407309554b
aalkhub10 <alkhubabdalhadi10@gmail.com>
2026-06-15 15:53:38 +1000

    initial default card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,39 @@
-const Card = ({ children, className} : {children: React.ReactNode, className?: string}) => {
+import {ReactNode } from "react";
+import {clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+interface CardProps { children?: React.ReactNode, className?: string, footer?: React.ReactNode, footerClassName?: string }
+
+const Card = ({ 
+    children = <p className= "text-gray-400"> default card component </p>, 
+    className, 
+    footer = <p className= "text-gray-400"> default footer component </p>,
+    footerClassName,
+
+    } : CardProps) => {
+
+    const defaultStyle = "flex flex-col bg-white border border-gray-400 p-6 rounded-lg shadow-md";
+    const defaultFooterStyle = "mt-auto pt-4 border-t border-gray-100 text-sm";
+
     return (
-        <div className={className}>
+
+        <div className={cn(defaultStyle, className)}>
+            <div className="flex-1">
             {children}
         </div>
+
+        <footer className={cn(defaultFooterStyle, footerClassName)}> 
+            <div>
+            {footer}
+            </div>
+        </footer>
+
+        </ div>
     );
 };
 
--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -3,21 +3,23 @@ import Card from '../components/Card';
 
 const HomeScreen = () => {
     return (
-        <div className="min-h-screen bg-slate-50 p-8">
-        <header className="flex flex-row items-center justify-center pt-20">
-            <h1 className="font-kufi text-6xl font-extrabold tracking-tight text-black mb-4 text-center">
-            Khawarizmia-Visualizer
-            </h1>
-        </header>
-        
-        <div className="grid grid-cols-4 gap-4" >
+        <div className="min-h-screen bg-slate-50 p-6">
+            <header className="items-center justify-center pt-10 pb-15">
+                <h1 className="font-kufi text-6xl font-extrabold tracking-tight text-black text-center">
+                    Khawarizmia-Visualizer
+                </h1>
+            </header>
 
-        <Card>
+            <div className="grid grid-cols-4 gap-4" >
+                <Card />
 
-        </Card>
+                <Card />
 
-        </div>
-       
+                <Card footer={<p >not default</p>}>
+                    <h2 >not default</h2>
+                </Card>
+            </div>
+
         </div>
     );
 };
