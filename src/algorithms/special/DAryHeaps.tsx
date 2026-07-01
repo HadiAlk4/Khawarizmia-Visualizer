2d4f08fcbe6098f5b09538bb363a639e81449d68
aalkhub10 <alkhubabdalhadi10@gmail.com>
2026-07-01 11:47:39 +1000

    added centering for nodes

--- src/algorithms/special/DAryHeaps.tsx
+++ src/algorithms/special/DAryHeaps.tsx
@@ -1,16 +1,120 @@
-import React, { useState } from "react";
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { ReactFlow, Background, Controls, Position } from "@xyflow/react";
+import type { Node, Edge } from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+
 
 const DAryHeaps = () => 
 {
     const [dValue, setDValue] = useState<number>(3);
     const [heapArray, setHeapArray] = useState<number[]> ([45, 20, 14, 12, 31, 7, 11, 2]);
+    const [newValue, setNewValue] = useState<string>("");
+
+    const [nodes, setNodes] = useState<Node[]>([]);
+    const [edges, setEdges] = useState<Edge[]>([]);
 
     const getParentIndex = (index: number) => 
     {
         if(index === 0) return null;
         return Math.floor((index - 1) / dValue);
     };
+
+    const handleInsert = () => 
+    {
+        if (heapArray.length >= 14) return;
+
+        if(!newValue) return;
+        const val = parseInt(newValue);
+        if(isNaN(val)) return;
+
+        const newHeap = [...heapArray, val];
+        let currIdx = newHeap.length - 1;
+        let parentIdx = Math.floor((currIdx - 1) / dValue);
+
+        while (currIdx > 0 && newHeap[currIdx] > newHeap[parentIdx])
+        {
+            const temp = newHeap[currIdx];
+            newHeap[currIdx] = newHeap[parentIdx];
+            newHeap[parentIdx] = temp;
+
+            currIdx = parentIdx;
+            parentIdx = Math.floor((currIdx - 1) / dValue);
+        }
+
+        setHeapArray(newHeap);
+        setNewValue("");
+
+    }
+
+    useEffect(() => {
+        const newNodes: Node[] = [];
+        const newEdges: Edge[] = [];
+
+        const xPosition: Record<number, number> = {};
+
+        heapArray.forEach((value, index) => {
+            let depth = 0;
+            let curr = index;
+            while (curr > 0){
+                curr = Math.floor((curr - 1) / dValue);
+                depth++;
+            }
+
+            let xPos = 0;
+            if(index === 0)
+            {
+                xPos = 0;
+            }  else 
+            {
+                const parentIdx = Math.floor((index - 1) / dValue);
+                const parentX = xPosition[parentIdx] || 0;
+
+                const childPosition = (index - 1) % dValue;
+
+                const centerOffset = (dValue - 1) / 2;
+                const shift = childPosition - centerOffset;
+
+                const spacing = 400 / Math.pow(dValue, depth - 1);
+
+                xPos = parentX + (shift * spacing);
+            }
+
+            xPosition[index] = xPos;
+
+            newNodes.push({ 
+                id: index.toString(),
+                position: { x: xPos, y: depth * 120},
+                data: {label: value.toString() },
+
+                style: {
+                    background: '#94ce4e',
+                    border: '2px solid black',
+                    fontWeight: 'bold',
+                    boxShadow: '4px 4px 0px 0px rgba(0,0,0,1)',
+                    borderRadius: 0,
+                    width: 50,
+                    display: 'flex',
+                    justifyContent: 'center'
+                }
+            });
+
+            if(index > 0){
+                const parentIdx = getParentIndex(index);
+                newEdges.push({
+                    id: `e${parentIdx}-${index}`,
+                    source: parentIdx?.toString() || "0",
+                    target: index.toString(),
+                    type: 'straight',
+                    style: {stroke: 'black', strokeWidth: 3}
+                });
+            }
+        });
+
+        setNodes(newNodes);
+        setEdges(newEdges);
+    }, [heapArray, dValue]);
 
     const getChildrenIndices = (index: number) => 
     {
@@ -47,6 +151,22 @@ const DAryHeaps = () =>
                 onChange={(e) => setDValue(parseInt(e.target.value) || 2)}
                 className="p-2 border-2 border-black font-bold text-xl outline-none focus:border-[#94ce4e] w-24 text-center"
                 />
+            </div>
+
+            
+
+        </div>
+
+        <div className="w-full max-w-5xl grid grid-cols-1 gap-8 mt-4">
+            <div className="w-full h-[400px] bg-white border-4 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">   
+            <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            fitView
+            >
+                <Background color="#ccc" gap={16} />
+                <Controls />
+            </ReactFlow>
             </div>
 
         </div>
